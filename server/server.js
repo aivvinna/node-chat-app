705c03169603dc83b52b42d6ae38b22bc51eaaae
Hiroki Furuichi <aivvinna@gmail.com>
2018-12-06 01:53:40 +0900

    added sockets on and emit for messages

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
+  socket.emit('newMessage', {
+    from: "example@email.com",
+    text: "Hello",
+    createdAt: 123
+  });
+
+  socket.on('createMessage', function (message) {
+    console.log('created message', message)
+  })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
