36d9daf5a4c1793b233d639032bb424438910e34
Hiroki Furuichi <aivvinna@gmail.com>
2018-12-07 01:54:58 +0900

    new users are greeted, other users are alerted when new user joins

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat Room',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user has joined the chat',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', function (message) {
     console.log('created message', message)
@@ -22,7 +33,12 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()  
+    // })
   })
 
   socket.on('disconnect', () => {
