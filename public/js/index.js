61f51d7b75186017af5beb1c720934b65bd8a4db
Hiroki Furuichi <aivvinna@gmail.com>
2018-12-08 00:29:27 +0900

    useres can now send a link with their location. also added test to make sure the location object is correct

--- public/js/index.js
+++ public/js/index.js
@@ -12,10 +12,20 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('New message', message);
   const li = jQuery('<li ></li>');
-  li.text(`${message.from}: ${message.text}`);
 
-  jQuery('#messages').append(li)
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 });
+
+socket.on('newLocationMessage', function (message) {
+  const li = jQuery('<li></li>');
+  const a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li)
+})
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
@@ -26,4 +36,20 @@ jQuery('#message-form').on('submit', function(e) {
   }, function() {
 
   });
+});
+
+const locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location.');
+  })
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +10,20 @@ describe('generateMessage', () => {
 
     expect(typeof(message.createdAt)).toBe('number');
     expect(message).toMatchObject({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Someone';
+    const latitude = '35';
+    const longitude = '139';
+    const locationMessage = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof(locationMessage.createdAt)).toBe('number');
+    expect(locationMessage).toMatchObject({
+      from,
+      url: `https://www.google.com/maps?q=${latitude},${longitude}`
+    });
   });
 });
